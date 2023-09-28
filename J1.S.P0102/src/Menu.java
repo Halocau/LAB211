a53cadec767adbdd8abd1258ada3e7db9a8c89de
Halocau <127236949+Halocau@users.noreply.github.com>
2023-09-29 01:12:43 +0700

    Add files via upload

---
+++ J1.S.P0102/src/Menu.java
@@ -0,0 +1,12 @@
+
+import java.util.Date;
+
+public class Menu {
+
+    public void display() {
+        Validation vaild = new Validation();
+        DateProcess dateProcess = new DateProcess();
+        Date enterDate = vaild.inputDate("Please enter date with format [dd/mm/yyyy]:");
+        System.out.println("Your day is " + dateProcess.getDayOfWeek(enterDate));
+    }
+}
