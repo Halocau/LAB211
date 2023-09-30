41f7c8a50bee2b8da388a1bf2af7a9f976881031
Halocau <127236949+Halocau@users.noreply.github.com>
2023-09-30 11:53:14 +0700

    Update Validation.java

--- J1.S.P0102/src/Validation.java
+++ J1.S.P0102/src/Validation.java
@@ -30,8 +30,8 @@ public class Validation {
             }
             // Nếu đúng fomat là số
             else if(!checkContainCharacter(input)){
-                // \d{1,2}: có thể có 1 hoặc 2 số
-                // \d{4} : có 4 số
+                // \d{1,2}: có thể có 1 hoặc 2 số ((ngày và tháng))
+                // \d{4} : có 4 số (năm)
                 // [/] : chứa cái kí tự /
                 
                 String patternDate = "\\d{1,2}[/]\\d{1,2}[/]\\d{4}";
