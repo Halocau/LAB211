97e4a49fbc8d5e1b5ca2a2fb0873603acee79dad
Halocau <127236949+Halocau@users.noreply.github.com>
2023-10-02 15:31:55 +0700

    Update DateProcess.java

--- J1.S.P.0103/src/J1SP0103/DateProcess.java
+++ J1.S.P.0103/src/J1SP0103/DateProcess.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package J1SP0103;
 
 import java.util.Date;
 
-/**
- *
- * @author Admin
- */
 public class DateProcess {
 
     public int compareDate(Date firstDate, Date secondDate) {
